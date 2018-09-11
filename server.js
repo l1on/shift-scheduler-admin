646184394dc455a18dc415943c4b5f18ab853d46
lion <>
2018-09-11 17:26:19 -0600

    added amdin service

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const http = require('http');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const terminus = require('@godaddy/terminus');
+
+process.env.NODE_ENV === "development" ? app.use(morgan('dev')) : app.use(morgan('combined'))
+
+const employees = [{"id":1,"name":"Lanny McDonald"},{"id":2,"name":"Allen Pitts"},{"id":3,"name":"Gary Roberts"},{"id":4,"name":"Dave Sapunjis"},{"id":5,"name":"Mike Vernon"}]
+app.get('/api/employees', (req, res) => {
+    res.json(employees);
+});
+app.get('/api/employees/:id', (req, res) => {
+    res.json(employees[req.params.id - 1]);
+});
+
+app.get('/api/shift-rules', (req, res) => {
+    res.json([{"rule_id":4,"employee_id":1,"value":3},{"rule_id":4,"employee_id":2,"value":5},{"rule_id":2,"employee_id":1,"value":5},{"rule_id":2,"employee_id":2,"value":5},{"rule_id":2,"value":6},{"rule_id":4,"value":2},{"rule_id":7,"value":2}]);
+});
+
+app.get('/api/time-offs', (req, res) => {
+    res.json([{"employee_id":1,"week":23,"days":[1,2,3]},{"employee_id":2,"week":23,"days":[5,6,7]},{"employee_id":3,"week":23,"days":[6,7]},{"employee_id":4,"week":24,"days":[3,4]},{"employee_id":5,"week":24,"days":[5,6,7]},{"employee_id":4,"week":24,"days":[1]},{"employee_id":1,"week":25,"days":[1,2,3]},{"employee_id":1,"week":25,"days":[7]},{"employee_id":4,"week":25,"days":[5,6,7]},{"employee_id":3,"week":25,"days":[6,7]},{"employee_id":5,"week":26,"days":[1,2,3]},{"employee_id":2,"week":26,"days":[3,4]},{"employee_id":4,"week":26,"days":[1,2,3,4]},{"employee_id":2,"week":26,"days":[1]}]);
+});
+
+const server = http.createServer(app);
+
+terminus(server, {
+    healthChecks: {
+        '/healthcheck': async () => {
+            return Promise.resolve()
+        },
+    },
+});
+
+server.listen(process.env.PORT);
