e0553b0a261154e5ed10b015d38bc3415d172741
lion <>
2018-09-14 14:17:09 -0600

    goggo

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const terminus = require('@godaddy/terminus');
 process.env.NODE_ENV === "development" ? app.use(morgan('dev')) : app.use(morgan('combined'))
 
 const employees = [{"id":1,"name":"Lanny McDonald"},{"id":2,"name":"Allen Pitts"},{"id":3,"name":"Gary Roberts"},{"id":4,"name":"Dave Sapunjis"},{"id":5,"name":"Mike Vernon"}]
-app.get('/api/admin/employees', (req, res) => {
+app.get('/admin/employees', (req, res) => {
     res.json(employees);
 });
-app.get('/api/admin/employees/:id', (req, res) => {
+app.get('/admin/employees/:id', (req, res) => {
     res.json(employees[req.params.id - 1]);
 });
 
-app.get('/api/admin/shift-rules', (req, res) => {
+app.get('/admin/shift-rules', (req, res) => {
     res.json([{"rule_id":4,"employee_id":1,"value":3},{"rule_id":4,"employee_id":2,"value":5},{"rule_id":2,"employee_id":1,"value":5},{"rule_id":2,"employee_id":2,"value":5},{"rule_id":2,"value":6},{"rule_id":4,"value":2},{"rule_id":7,"value":2}]);
 });
 
-app.get('/api/admin/time-offs', (req, res) => {
+app.get('/admin/time-offs', (req, res) => {
     res.json([{"employee_id":1,"week":23,"days":[1,2,3]},{"employee_id":2,"week":23,"days":[5,6,7]},{"employee_id":3,"week":23,"days":[6,7]},{"employee_id":4,"week":24,"days":[3,4]},{"employee_id":5,"week":24,"days":[5,6,7]},{"employee_id":4,"week":24,"days":[1]},{"employee_id":1,"week":25,"days":[1,2,3]},{"employee_id":1,"week":25,"days":[7]},{"employee_id":4,"week":25,"days":[5,6,7]},{"employee_id":3,"week":25,"days":[6,7]},{"employee_id":5,"week":26,"days":[1,2,3]},{"employee_id":2,"week":26,"days":[3,4]},{"employee_id":4,"week":26,"days":[1,2,3,4]},{"employee_id":2,"week":26,"days":[1]}]);
 });
 
